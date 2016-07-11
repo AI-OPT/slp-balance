206a0cddb113cd041777530eab6c40c1875f1e06
zhangzhongde <xtgjzzd@163.com>
2016-07-11 18:10:15 +0800

    dingdanruzhang

--- balance-core/src/main/java/com/ai/slp/balance/service/atom/impl/BillAccountAtomSVImpl.java
+++ balance-core/src/main/java/com/ai/slp/balance/service/atom/impl/BillAccountAtomSVImpl.java
@@ -3,6 +3,7 @@ package com.ai.slp.balance.service.atom.impl;
 import org.springframework.stereotype.Component;
 
 import com.ai.slp.balance.dao.mapper.bo.BillAccount;
+import com.ai.slp.balance.dao.mapper.bo.BillAccountKey;
 import com.ai.slp.balance.dao.mapper.factory.MapperFactory;
 import com.ai.slp.balance.service.atom.interfaces.IBillAccountAtomSV;
 @Component
@@ -12,6 +13,18 @@ public class BillAccountAtomSVImpl implements IBillAccountAtomSV {
 	public void insert(BillAccount billAccount) {
 
 		MapperFactory.getBillAccountMapper().insert(billAccount);
+	}
+
+	@Override
+	public BillAccount getBillAccount(BillAccountKey billAccountKey) {
+		return MapperFactory.getBillAccountMapper().selectByPrimaryKey(billAccountKey);
+		
+	}
+
+	@Override
+	public void updateBillAccountByPrimaryKeySelective(BillAccount billAccount) {
+
+		MapperFactory.getBillAccountMapper().updateByPrimaryKeySelective(billAccount);
 	}
 
 }
--- balance-core/src/main/java/com/ai/slp/balance/service/atom/interfaces/IBillAccountAtomSV.java
+++ balance-core/src/main/java/com/ai/slp/balance/service/atom/interfaces/IBillAccountAtomSV.java
@@ -1,7 +1,25 @@
 package com.ai.slp.balance.service.atom.interfaces;
 
 import com.ai.slp.balance.dao.mapper.bo.BillAccount;
+import com.ai.slp.balance.dao.mapper.bo.BillAccountKey;
 
 public interface IBillAccountAtomSV {
 	public void insert(BillAccount billAccount);
+	/**
+	 * 根据主键查询信息是否存在
+	 * @param billAccountKey
+	 * @return
+	 * @author zhangzd
+	 * @ApiDocMethod
+	 * @ApiCode
+	 */
+	public BillAccount getBillAccount(BillAccountKey billAccountKey);
+	/**
+	 * 通过主键修改信息
+	 * @param billAccount
+	 * @author zhangzd
+	 * @ApiDocMethod
+	 * @ApiCode
+	 */
+	public void updateBillAccountByPrimaryKeySelective(BillAccount billAccount);
 }
