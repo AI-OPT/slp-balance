0a8755250fdfdc9f56df4108ba8306733be865de
zhangzhongde <xtgjzzd@163.com>
2016-07-19 15:21:02 +0800

    订单入账校验

--- balance-core/src/main/java/com/ai/slp/balance/api/ordertobillaccount/impl/OrderToBillAccountSVImpl.java
+++ balance-core/src/main/java/com/ai/slp/balance/api/ordertobillaccount/impl/OrderToBillAccountSVImpl.java
@@ -7,6 +7,7 @@ import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.opt.base.vo.BaseResponse;
 import com.ai.opt.base.vo.ResponseHeader;
+import com.ai.opt.sdk.util.StringUtil;
 import com.ai.slp.balance.api.ordertobillaccount.interfaces.IOrderToBillAccountSV;
 import com.ai.slp.balance.api.ordertobillaccount.param.BillGenRequest;
 import com.ai.slp.balance.service.business.interfaces.IBillAccountBusiSV;
@@ -22,6 +23,29 @@ public class OrderToBillAccountSVImpl implements IOrderToBillAccountSV {
 	public BaseResponse orderToBillAccount(BillGenRequest billgenRequest) throws BusinessException, SystemException {
 		BaseResponse response =  new BaseResponse();
 		ResponseHeader responseHeader = new ResponseHeader();
+		
+		if(StringUtil.isBlank(billgenRequest.getAccountId())){
+			throw new BusinessException("8888","账户id不能为空");
+		}
+		if(StringUtil.isBlank(billgenRequest.getProductCatId())){
+			throw new BusinessException("8888","商品类目id不能为空");
+		}
+		if(StringUtil.isBlank(billgenRequest.getTenantId())){
+			throw new BusinessException("8888","租户id不能为空");
+		}
+		if(StringUtil.isBlank(billgenRequest.getUserId())){
+			throw new BusinessException("8888","用户id不能为空");
+		}
+		if(null != billgenRequest.getFee() && StringUtil.isBlank(String.valueOf(billgenRequest.getFee()))){
+			throw new BusinessException("8888","订单费用不能为空");
+		}
+		if(null != billgenRequest.getOverdraftQuota() && StringUtil.isBlank(String.valueOf(billgenRequest.getOverdraftQuota()))){
+			throw new BusinessException("8888","透支额不能为空");
+		}
+		
+		if(null != billgenRequest.getOrderTime()){
+			throw new BusinessException("8888","下单时间不能为空");
+		}
 		//
 		try{
 			this.billAccountBusiSV.orderToBillAccount(billgenRequest);
