6c361923b05e57b2f937807585ed79b8e7e30b39
zhangzhongde <xtgjzzd@163.com>
2016-07-19 14:48:53 +0800

    订单入账接口校验

--- balance-core/src/main/java/com/ai/slp/balance/api/ordertobillaccount/impl/OrderToBillAccountSVImpl.java
+++ balance-core/src/main/java/com/ai/slp/balance/api/ordertobillaccount/impl/OrderToBillAccountSVImpl.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
+import com.ai.opt.base.vo.BaseResponse;
+import com.ai.opt.base.vo.ResponseHeader;
 import com.ai.slp.balance.api.ordertobillaccount.interfaces.IOrderToBillAccountSV;
 import com.ai.slp.balance.api.ordertobillaccount.param.BillGenRequest;
 import com.ai.slp.balance.service.business.interfaces.IBillAccountBusiSV;
@@ -15,14 +17,28 @@ public class OrderToBillAccountSVImpl implements IOrderToBillAccountSV {
 	
 	@Autowired
 	private IBillAccountBusiSV billAccountBusiSV; 
-	public OrderToBillAccountSVImpl() {
-		// TODO Auto-generated constructor stub
-	}
 	
 	@Override
-	public void orderToBillAccount(BillGenRequest billgenRequest) throws BusinessException, SystemException {
-		
-		this.billAccountBusiSV.orderToBillAccount(billgenRequest);
+	public BaseResponse orderToBillAccount(BillGenRequest billgenRequest) throws BusinessException, SystemException {
+		BaseResponse response =  new BaseResponse();
+		ResponseHeader responseHeader = new ResponseHeader();
+		//
+		try{
+			this.billAccountBusiSV.orderToBillAccount(billgenRequest);
+			responseHeader.setIsSuccess(true);
+			responseHeader.setResultCode("000000");
+			responseHeader.setResultMessage("订单入账成功");
+			response.setResponseHeader(responseHeader);
+			//
+			return response;
+		}catch(Exception e){
+			responseHeader.setIsSuccess(false);
+			responseHeader.setResultCode("000001");
+			responseHeader.setResultMessage("订单入账失败");
+			response.setResponseHeader(responseHeader);
+			//
+			return response;
+		}		
 	}
 
 }
