ba52d0e55b7f91397e14616afce5b6bc4e0f90ce
zhangzhongde <xtgjzzd@163.com>
2016-07-22 10:23:57 +0800

    修改测试类

--- balance-core/src/test/java/com/ai/slp/balance/test/api/resdeduct/impl/ResDeductTest.java
+++ balance-core/src/test/java/com/ai/slp/balance/test/api/resdeduct/impl/ResDeductTest.java
@@ -1,8 +1,8 @@
 package com.ai.slp.balance.test.api.resdeduct.impl;
 
 import junit.framework.TestCase;
-import net.sourceforge.groboutils.junit.v1.MultiThreadedTestRunner;
-import net.sourceforge.groboutils.junit.v1.TestRunnable;
+//import net.sourceforge.groboutils.junit.v1.MultiThreadedTestRunner;
+//import net.sourceforge.groboutils.junit.v1.TestRunnable;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -44,37 +44,37 @@ public class ResDeductTest extends TestCase {
     @Test
     public void testMultiRequests() {
         // 构造一个Runner
-        TestRunnable runner = new TestRunnable() {
-            @Override
-            public void runTest() throws Throwable {
-                // 测试内容
-                ResourceDeduct param = new ResourceDeduct();
-                param.setTenantId("BIS-ST");
-                param.setOwnerId(10);
-                param.setOwnerType(0);
-                Long seq = SeqUtil.getNewId(SeqConstants.FUN_FUND_SERIAL$PAY_SERIAL_CODE);
-                log.error("线程{}获取到的SEQ:{}",Thread.currentThread().getName(),seq);
-                param.setExternalId(seq.toString());
-                param.setSystemId("Runner-BIS");
-                param.setTotalAmount(1);
-                param.setResourceType(60);
-                log.info("请求参数:"+JSON.toJSON(param));
-                //resDeductSV.deductResource(param);
-            }
-        };
-        int runnerCount = 100;
-        // Runner数组，想当于并发多少个。
-        TestRunnable[] trs = new TestRunnable[runnerCount];
-        for (int i = 0; i < runnerCount; i++) {
-            trs[i] = runner;
-        }
-        // 用于执行多线程测试用例的Runner，将前面定义的单个Runner组成的数组传入
-        MultiThreadedTestRunner mttr = new MultiThreadedTestRunner(trs);
-        try {
-            // 开发并发执行数组里定义的内容
-            mttr.runTestRunnables();
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
+//        TestRunnable runner = new TestRunnable() {
+//            @Override
+//            public void runTest() throws Throwable {
+//                // 测试内容
+//                ResourceDeduct param = new ResourceDeduct();
+//                param.setTenantId("BIS-ST");
+//                param.setOwnerId(10);
+//                param.setOwnerType(0);
+//                Long seq = SeqUtil.getNewId(SeqConstants.FUN_FUND_SERIAL$PAY_SERIAL_CODE);
+//                log.error("线程{}获取到的SEQ:{}",Thread.currentThread().getName(),seq);
+//                param.setExternalId(seq.toString());
+//                param.setSystemId("Runner-BIS");
+//                param.setTotalAmount(1);
+//                param.setResourceType(60);
+//                log.info("请求参数:"+JSON.toJSON(param));
+//                //resDeductSV.deductResource(param);
+//            }
+//        };
+//        int runnerCount = 100;
+//        // Runner数组，想当于并发多少个。
+//        TestRunnable[] trs = new TestRunnable[runnerCount];
+//        for (int i = 0; i < runnerCount; i++) {
+//            trs[i] = runner;
+//        }
+//        // 用于执行多线程测试用例的Runner，将前面定义的单个Runner组成的数组传入
+//        MultiThreadedTestRunner mttr = new MultiThreadedTestRunner(trs);
+//        try {
+//            // 开发并发执行数组里定义的内容
+//            mttr.runTestRunnables();
+//        } catch (Throwable e) {
+//            e.printStackTrace();
+//        }
     }
 }
