df3328a7ffaa31b7d82483bb05def951e0886d7f
zhangzhongde <xtgjzzd@163.com>
2016-07-25 10:13:43 +0800

    查询信用额度修改记录 条件为update_mask字符串中第十一位为1的数据 ；信用额度详情查询；已使用额度查询；未使用额度查询

--- balance-core/src/test/java/com/ai/slp/balance/test/api/custcredit/impl/CustCreditManageTest.java
+++ balance-core/src/test/java/com/ai/slp/balance/test/api/custcredit/impl/CustCreditManageTest.java
@@ -15,6 +15,10 @@ import com.ai.slp.balance.api.custcredit.param.CustCreditDetailResponse;
 import com.ai.slp.balance.api.custcredit.param.CustCreditRequest;
 import com.ai.slp.balance.api.custcredit.param.CustCreditSettingRecordRequest;
 import com.ai.slp.balance.api.custcredit.param.CustCreditSettingRecordResponse;
+import com.ai.slp.balance.api.custcredit.param.CustCreditUnUsedRequest;
+import com.ai.slp.balance.api.custcredit.param.CustCreditUnUsedResponse;
+import com.ai.slp.balance.api.custcredit.param.CustCreditUsedRequest;
+import com.ai.slp.balance.api.custcredit.param.CustCreditUsedResponse;
 import com.alibaba.fastjson.JSON;
 
 import junit.framework.TestCase;
@@ -91,9 +95,48 @@ public class CustCreditManageTest  extends TestCase {
 		//
 		request.setAccountId(11151l);
 		request.setTenantId("SLP");
+		request.setPageNo(1);
+		request.setPageSize(5);
 		//
 		log.info("request:"+JSON.toJSONString(request));
 		CustCreditSettingRecordResponse response = this.custCreditManageSV.queryCustCreditSettingRecord(request);
+		log.info("response:"+JSON.toJSONString(response));
+	}
+	/**
+	 * 查询已使用额度
+	 * 
+	 * @author zhangzd
+	 * @ApiDocMethod
+	 * @ApiCode
+	 */
+	@Test
+	public void findCustCreditUsed(){
+		CustCreditUsedRequest request = new CustCreditUsedRequest();
+		//
+		request.setAccountId(11151l);
+		request.setTenantId("SLP");
+		//
+		log.info("request:"+JSON.toJSONString(request));
+		CustCreditUsedResponse response = this.custCreditManageSV.findCustCreditUsed(request);
+		log.info("response:"+JSON.toJSONString(response));
+	}
+	
+	/**
+	 * 查询未使用额度
+	 * 
+	 * @author zhangzd
+	 * @ApiDocMethod
+	 * @ApiCode
+	 */
+	@Test
+	public void findCustCreditUnUsed(){
+		CustCreditUnUsedRequest request = new CustCreditUnUsedRequest();
+		//
+		request.setAccountId(11151l);
+		request.setTenantId("SLP");
+		//
+		log.info("request:"+JSON.toJSONString(request));
+		CustCreditUnUsedResponse response = this.custCreditManageSV.findCustCreditUnUsed(request);
 		log.info("response:"+JSON.toJSONString(response));
 	}
 
