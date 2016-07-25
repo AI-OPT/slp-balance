150dba2825f0a0d0db1548a258af069d49faae3f
zhangzhongde <xtgjzzd@163.com>
2016-07-25 10:12:57 +0800

    查询信用额度修改记录 条件为update_mask字符串中第十一位为1的数据

--- balance-core/src/main/java/com/ai/slp/balance/service/atom/impl/FunAccountLogAtomSVImpl.java
+++ balance-core/src/main/java/com/ai/slp/balance/service/atom/impl/FunAccountLogAtomSVImpl.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Component;
 
 import com.ai.opt.base.vo.PageInfo;
 import com.ai.opt.sdk.util.StringUtil;
+import com.ai.slp.balance.constants.FunAccountLogConstants;
 import com.ai.slp.balance.dao.mapper.bo.FunAccountLog;
 import com.ai.slp.balance.dao.mapper.bo.FunAccountLogCriteria;
 import com.ai.slp.balance.dao.mapper.factory.MapperFactory;
 import com.ai.slp.balance.dao.mapper.interfaces.FunAccountLogMapper;
 import com.ai.slp.balance.service.atom.interfaces.IFunAccountLogAtomSV;
+import com.esotericsoftware.minlog.Log;
 
 /**
  * 账户信息历史记录表基础服务类
@@ -45,6 +47,14 @@ public class FunAccountLogAtomSVImpl implements IFunAccountLogAtomSV{
 		if(null != endTime){
 			criteria.andUpdateTimeLessThanOrEqualTo(endTime);
 		}
+		//
+		String str32 = FunAccountLogConstants.str32Zero();
+		StringBuffer stringBuffer = new StringBuffer(str32);
+		//将第十一位字符替换为1 当前修改信用额度
+		stringBuffer.replace(10, 11, "1");
+		Log.info("update_mask:"+stringBuffer.toString());
+		criteria.andUpdateMaskEqualTo(stringBuffer.toString());
+		//
 		//
 		example.setOrderByClause(" update_time desc");
 		if (pageNo != null && pageSize != null) {
